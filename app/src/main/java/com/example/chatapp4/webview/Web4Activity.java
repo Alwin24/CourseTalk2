c8ee6db86a5dfbd50ac34bd85d8926d27bd4f599
alwin <alwin24012002@gmail.com>
2021-11-28 19:27:17 +0530

    Rebase

---
+++ app/src/main/java/com/example/chatapp4/webview/Web4Activity.java
@@ -0,0 +1,23 @@
+package com.example.chatapp4.webview;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.chatapp4.R;
+
+public class Web4Activity extends AppCompatActivity {
+
+    WebView web;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_web4);
+        web = findViewById(R.id.webView);
+        web.setWebViewClient(new WebViewClient());
+        web.loadUrl("https://www.google.com/");
+    }
+}
